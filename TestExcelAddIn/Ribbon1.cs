726cf22ad31cdea0eaf21335b830e9bf305769bd
arielclubspark <ariel.garmendia@clubspark.com>
2022-07-14 12:00:49 +0200

    Add project files.

---
+++ TestExcelAddIn/Ribbon1.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Drawing;
+
+namespace TestExcelAddIn
+{
+    public partial class Ribbon1
+    {
+        private const int defaultInc = 5;
+
+        private Microsoft.Office.Tools.Excel.Controls.TextBox incremento;
+
+        private Microsoft.Office.Tools.Excel.Controls.DateTimePicker campoA;
+        private Microsoft.Office.Tools.Excel.Controls.DateTimePicker campoB;
+
+        private Microsoft.Office.Tools.Excel.Controls.Button reset;
+        private Microsoft.Office.Tools.Excel.Controls.Button incdays;
+
+        private Color incButtonColor;
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void RunButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            Setup();
+        }
+
+        private void Incremento_TextChanged(object sender, EventArgs e)
+        {
+            sumDays();
+        }
+
+        private void CampoA_ValueChanged(object sender, EventArgs e)
+        {
+            sumDays();
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void Incdays_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                incremento.Text = (Convert.ToInt32(incremento.Text) + 1).ToString();
+
+                if (Convert.ToInt32(incremento.Text) != defaultInc)
+                    incdays.BackColor = Color.Red;
+                else
+                    incdays.BackColor = incButtonColor;
+            }
+            catch
+            {
+                incremento.Text = defaultInc.ToString();
+            }
+        }
+
+        private void sumDays()
+        {
+            try
+            {
+                campoB.Value = campoA.Value.AddDays(Convert.ToInt32(incremento.Text));
+
+                if (Convert.ToInt32(incremento.Text) != defaultInc)
+                    incremento.BackColor = Color.Yellow;
+                else
+                    incremento.BackColor = Color.White;
+            }
+            catch
+            {
+                incremento.Text = defaultInc.ToString();
+            }
+        }
+    }
+}
---
+++ TestExcelAddIn/ThisAddIn.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
+namespace TestExcelAddIn
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            this.Application.WorkbookActivate += Application_WorkbookOpen;
+        }
+
+        private void Application_WorkbookOpen(Excel.Workbook Wb)
+        {
+            Wb.Sheets.Add(After: Wb.Sheets[Wb.Sheets.Count]);
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
